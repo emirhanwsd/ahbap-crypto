6b1dbbec5a5dc59d1aeced4b687770a5228cfa15
Furkan Kose <furkankosee@hotmail.com.tr>
2023-02-08 00:39:16 +0300

    Fix copy button localization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -157,7 +157,7 @@ const Home = () => {
                     }}
                     className="px-4 py-2 rounded bg-brand text-white shadow-sm font-medium inline-flex items-center justify-center text-sm hover:bg-green-600 transition-colors"
                   >
-                    Kopyala
+                    {t("copy")}
                   </button>
                 </div>
 
@@ -182,7 +182,7 @@ const Home = () => {
                     }}
                     className="px-4 py-2 rounded bg-brand text-white shadow-sm font-medium inline-flex items-center justify-center text-sm hover:bg-green-600 transition-colors"
                   >
-                    Kopyala
+                    {t("copy")}
                   </button>
                 </div>
 
@@ -207,7 +207,7 @@ const Home = () => {
                     }}
                     className="px-4 py-2 rounded bg-brand text-white shadow-sm font-medium inline-flex items-center justify-center text-sm hover:bg-green-600 transition-colors"
                   >
-                    Kopyala
+                    {t("copy")}
                   </button>
                 </div>
 
