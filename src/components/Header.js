f086d4b3ab600988596a111a7c74dbe101c013e6
Doga Genc <dogagenc@gmail.com>
2023-02-08 02:28:52 +0300

    Add all translations

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,26 @@ import { Menu, Transition } from "@headlessui/react";
 
 import ahbap from "../assets/ahbap.png";
 
-import tr from "../assets/flags/tr.png";
-import en from "../assets/flags/en.png";
-
 const Header = () => {
   const { t, i18n } = useTranslation();
 
+  const allLanguages = Object.keys(i18n.services.resourceStore.data);
+
+  const getFlagImage = (lang) => {
+    const flagMap = {
+      en: 'gb',
+      pt: 'br'
+    };
+
+    const flag = flagMap[lang] || lang;
+
+    return `https://country-flag-proxy.nod.workers.dev/png/${flag}`;
+  };
+
   return (
     <header>
-      <div className="bg-brand sm:h-[592px] pb-16 sm:pb-0 bg-brand text-white flex flex-col items-center text-center px-4 lg:px-0">
-        <img className="mt-24 sm:mt-10" src={ahbap} alt="" />
+      <div className="bg-brand sm:h-[592px] pb-16 sm:pb-0 text-white flex flex-col items-center text-center px-4 lg:px-0">
+        <img className="mt-24 sm:mt-10 w-[200px]" src={ahbap} alt="Ahbap logo" />
 
         <h1 className="text-xl sm:text-2xl font-semibold mt-8 max-w-7xl">
           {t("title")}
@@ -28,16 +38,14 @@ const Header = () => {
           className="absolute top-6 right-6 lg:right-24 inline-block text-left select-none"
         >
           <div>
-            <Menu.Button className="inline-flex w-full justify-center rounded border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none hover:opacity-90 transition-opacity">
+            <Menu.Button className="inline-flex w-full justify-center items-center rounded border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none hover:opacity-90 transition-opacity">
               <img
-                className="h-5 rounded-sm mr-2"
-                src={i18n.language === "en" ? en : tr}
-                alt=""
+                className="w-6 h-4 rounded-sm mr-2"
+                src={getFlagImage(i18n.language)}
+                alt={`i18n.language flag`}
               />
 
-              <span>
-                {i18n.language === "en" ? t("english") : t("turkish")}
-              </span>
+              <span class="uppercase">{i18n.language}</span>
 
               <ChevronDownIcon
                 className="-mr-1 ml-2 h-5 w-5"
@@ -55,43 +63,33 @@ const Header = () => {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 z-10 h-96 overflow-y-auto mt-2 w-28 origin-top-right rounded bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <div
-                      onClick={() => {
-                        i18n.changeLanguage("tr");
-                      }}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "px-4 py-2 text-sm flex items-center gap-x-2 cursor-pointer"
-                      )}
-                    >
-                      <img className="h-5 rounded-sm" src={tr} alt="" />
+                {allLanguages.map((languageCode, index) => (
+                  <Menu.Item key={index}>
+                    {({ active }) => (
+                      <div
+                        onClick={() => {
+                          i18n.changeLanguage(languageCode);
+                        }}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "px-4 py-2 text-sm flex items-center gap-x-2 cursor-pointer"
+                        )}
+                      >
+                        <img
+                          className="w-6 h-4 rounded-sm"
+                          src={getFlagImage(languageCode)}
+                          alt={`${languageCode} flag`}
+                        />
 
-                      <span>{t("turkish")}</span>
-                    </div>
-                  )}
-                </Menu.Item>
-
-                <Menu.Item>
-                  {({ active }) => (
-                    <div
-                      onClick={() => {
-                        i18n.changeLanguage("en");
-                      }}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "px-4 py-2 text-sm flex items-center gap-x-2 cursor-pointer"
-                      )}
-                    >
-                      <img className="h-5 rounded-sm" src={en} alt="" />
-
-                      <span>{t("english")}</span>
-                    </div>
-                  )}
-                </Menu.Item>
+                        <span class="uppercase">{languageCode}</span>
+                      </div>
+                    )}
+                  </Menu.Item>
+                ))}
               </div>
             </Menu.Items>
           </Transition>
