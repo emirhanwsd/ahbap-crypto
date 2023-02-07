29609d621363b735b933aabfad0291faf51e33c6
Emirhan Akpınar <leetpocketmine@gmail.com>
2023-02-07 17:32:55 +0300

    Design responsive

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,7 +1,7 @@
 const TableRow = (
   { image, a, b, c, d } // TODO: Name
 ) => (
-  <div className="py-4 flex items-center justify-between border-b">
+  <div className="py-4 flex flex-col sm:flex-row sm:items-center gap-y-4 justify-between border-b">
     <div className="flex items-center gap-x-2.5">
       <img className="h-8 w-8" src={image} alt="" />
 
@@ -11,7 +11,7 @@ const TableRow = (
       </div>
     </div>
 
-    <div className="flex flex-col">
+    <div className="text-sm sm:text-base flex flex-row sm:flex-col justify-between sm:justify-start">
       <span>{c}</span>
       <span>{d}</span>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -129,20 +129,20 @@ const Home = () => {
   return (
     <div className="pb-24">
       <header>
-        <div className="h-[589px] bg-black text-white flex flex-col items-center text-center">
-          <img className="mt-10" src={ahbap} alt="" />
+        <div className="sm:h-[589px] pb-16 sm:pb-0 bg-black text-white flex flex-col items-center text-center px-4 lg:px-0">
+          <img className="mt-24 sm:mt-10" src={ahbap} alt="" />
 
-          <h1 className="text-2xl font-semibold mt-8 max-w-7xl">
+          <h1 className="text-xl sm:text-2xl font-semibold mt-8 max-w-7xl">
             {t("Turkiye buyuk depremlerle sarsiliyor")}
           </h1>
 
-          <h2 className="text-xl mt-8 max-w-3xl">
+          <h2 className="text-base sm:text-xl mt-8 max-w-3xl">
             {t("Merhaba, Ahbap araciligi ile kripto bagis topluyoruz.")}
           </h2>
 
           <Menu
             as="div"
-            className="absolute top-6 right-24 inline-block text-left"
+            className="absolute top-6 right-6 lg:right-24 inline-block text-left select-none"
           >
             <div>
               <Menu.Button className="inline-flex w-full justify-center rounded border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none hover:opacity-90 transition-opacity">
@@ -219,14 +219,14 @@ const Home = () => {
           </Menu>
         </div>
 
-        <div className="mt-12 flex gap-x-6 h-[420px] justify-center -mt-40">
-          <div className="bg-white w-[565px] border rounded shadow px-5 py-8 flex flex-col text-black text-left">
+        <div className="mt-12 flex flex-col lg:flex-row gap-6 lg:h-[420px] justify-center lg:-mt-40 px-8 lg:px-0">
+          <div className="bg-white lg:w-[565px] border rounded shadow px-5 py-8 flex flex-col text-black text-left">
             <h1 className="font-bold text-2xl">
               {t("Toplanan Bagis Miktari")}
             </h1>
 
-            <div className="mt-4 flex items-center gap-x-5">
-              <div className="px-8 py-6 inline-flex items-center justify-center rounded border border-brand text-brand text-2xl font-bold">
+            <div className="mt-4 flex flex-col sm:flex-row sm:items-center gap-5">
+              <div className="w-full sm:w-auto px-8 py-4 sm:py-6 inline-flex items-center justify-center rounded border border-brand text-brand text-2xl font-bold">
                 $157.082 BNB
               </div>
 
@@ -242,12 +242,12 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="bg-white w-[565px] border rounded shadow px-5 py-8 flex flex-col text-black text-left">
+          <div className="bg-white lg:w-[565px] border rounded shadow px-5 py-8 flex flex-col text-black text-left">
             <h1 className="font-bold text-2xl">
               {t("Transfer Yapilirken Dikkat Edilmesi Gerekenler")}
             </h1>
 
-            <div className="mt-6 grid grid-cols-3">
+            <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-8">
               <div className="space-y-2">
                 <h1 className="font-bold">
                   {t("Kripto Agi", {
@@ -301,49 +301,7 @@ const Home = () => {
         </div>
       </header>
 
-      {/*<div className="mt-24 mx-auto max-w-3xl bg-white border rounded shadow p-5 flex flex-col text-black text-left">*/}
-      {/*  <h1 className="font-bold text-2xl">*/}
-      {/*    Transfer Yapılırken Dikkat Edilmesi Gerekenler*/}
-      {/*  </h1>*/}
-
-      {/*  <div className="mt-8 grid grid-cols-3">*/}
-      {/*    <div className="space-y-2">*/}
-      {/*      <h1 className="font-bold">ERC20 Ağı</h1>*/}
-
-      {/*      <div className="flex flex-col text-sm">*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*        <span>Tether USD (USDT)</span>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-
-      {/*    <div className="space-y-2">*/}
-      {/*      <h1 className="font-bold">AVALANCHE Ağı</h1>*/}
-
-      {/*      <div className="flex flex-col text-sm">*/}
-      {/*        <span>Chainlink (LINK)</span>*/}
-      {/*        <span>Chainlink (LINK)</span>*/}
-      {/*        <span>Chainlink (LINK)</span>*/}
-      {/*      </div>*/}
-      {/*    </div>*/}
-
-      {/*    <div className="space-y-2">*/}
-      {/*      <h1 className="font-bold">BEP20 Ağı</h1>*/}
-
-      {/*      <p className="">*/}
-      {/*        Tüm coinler destekleniyor fakat yollarken BEP20 ağını seçin.*/}
-      {/*      </p>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-      <div className="mt-[72px] flex flex-col gap-y-2 max-w-6xl mx-auto">
+      <div className="mt-[72px] flex flex-col gap-y-2 max-w-6xl mx-auto px-8 lg:px-0">
         <h1 className="text-2xl font-bold">
           {t("Hangi adrese ne kadar bagis yapildi?")}
         </h1>
@@ -354,7 +312,7 @@ const Home = () => {
           ))}
         </div>
 
-        <div className="mt-6 self-end flex items-center gap-x-2">
+        <div className="mt-6 self-center sm:self-end flex items-center gap-x-2">
           {[1, 2, 3, 4, 5, 6].map((_) => (
             <div
               onClick={() => setPage(_)}
