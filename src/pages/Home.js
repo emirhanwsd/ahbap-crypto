13274763ae319843b1de779f89be9a4c1c17b164
Aykut Kardaş <aykutkrds@gmail.com>
2023-02-07 18:12:11 +0300

    Update translation key

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,61 +1,56 @@
+import { Fragment, useEffect, useState } from "react";
+import { useTranslation, initReactI18next } from "react-i18next";
+import classNames from "classnames";
+import i18n from "i18next";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Menu, Transition } from "@headlessui/react";
+
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+
 import ahbap from "../assets/ahbap.png";
 import binance from "../assets/binance.png";
 import TableRow from "../components/TableRow";
-import { Fragment, useEffect, useState } from "react";
-import classNames from "classnames";
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Menu, Transition } from "@headlessui/react";
+
 import tr from "../assets/flags/tr.png";
 import en from "../assets/flags/en.png";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
       translation: {
-        "Turkiye buyuk depremlerle sarsiliyor":
+        title:
           "Turkey is being effected by destructive earthquakes. 10,000 buildings were collapsed. There are 50,000 people under rubble. 13 million people are waiting for help. You can send your help via the following cryptocurrency wallet addresses.",
-        "Merhaba, Ahbap araciligi ile kripto bagis topluyoruz.":
+        subTitle:
           "Hello, we are collecting crypto donations through Ahbap to help earthquake victims in Turkey. You can use the addresses below to donate cryptocurrency to support this cause.",
         "Toplanan Bagis Miktari": "Total Amount of Donation",
-        Bakiye: "Balance",
-        "BNB Degeri": "BNB Value",
-        "Bagis yapacaginiz kripto adresleri:": "Crypto addresses to donate:",
-        "Transfer Yapilirken Dikkat Edilmesi Gerekenler":
-          "Things to Consider While Making a Transfer",
-        "Kripto Agi": "{{kripto}} Network",
-        "Tum coinler destekleniyor fakat yollarken BEP20 agini secin":
-          "All coins are supported but select BEP20 network while sending.",
-        "Hangi adrese ne kadar bagis yapildi?":
-          "How much was donated to which address?",
-        Sayfa: "Page",
-        Turkce: "Turkish",
-        Ingilizce: "English",
+        balance: "Balance",
+        bnbValue: "BNB Value",
+        thingsToConsider: "Things to Consider While Making a Transfer",
+        cryptoNetwork: "{{kripto}} Network",
+        cryptoNetworkWarning: "All coins are supported but select BEP20 network while sending.",
+        whichAddress: "How much was donated to which address?",
+        page: "Page",
+        turkish: "Turkish",
+        english: "English",
       },
     },
     tr: {
       translation: {
-        "Turkiye buyuk depremlerle sarsiliyor":
+        title:
           "Türkiye büyük depremlerle sarsılıyor. 10.000 bina yıkıldı. Göcük altında 50.000 kişi var. 13 milyon insan yardım bekliyor. Aşağıdaki kriptopara cüzdan adresleri üzerinden yardımlarınızı iletebilirsiniz.",
-        "Merhaba, Ahbap araciligi ile kripto bagis topluyoruz.":
+        subTitle:
           "Merhabalar, Türkiye’de yaşanan deprem afetine yardımcı olmak için Ahbap aracılığı ile kripto bağış topluyoruz. Bu bağışa kripto para ile destek olmak için aşağıdaki adresleri kullanabiliriz.",
         "Toplanan Bagis Miktari": "Toplanan Bağış Miktarı",
-        Bakiye: "Bakiye",
-        "BNB Degeri": "BNB Değeri",
-        "Bagis yapacaginiz kripto adresleri:":
-          "Bağış yapacağınız kripto adresleri:",
-        "Transfer Yapilirken Dikkat Edilmesi Gerekenler":
-          "Transfer Yapılırken Dikkat Edilmesi Gerekenler",
-        "Kripto Agi": "{{kripto}} Ağı",
-        "Tum coinler destekleniyor fakat yollarken BEP20 agini secin":
+        balance: "Bakiye",
+        bnbValue: "BNB Değeri",
+        thingsToConsider: "Transfer Yapılırken Dikkat Edilmesi Gerekenler",
+        cryptoNetwork: "{{kripto}} Ağı",
+        cryptoNetworkWarning:
           "Tüm coinler destekleniyor fakat yollarken BEP20 ağını seçin.",
-        "Hangi adrese ne kadar bagis yapildi?":
-          "Hangi adrese ne kadar bağış yapıldı?",
-        Sayfa: "Sayfa",
-        Turkce: "Türkçe",
-        Ingilizce: "İngilizce",
+        whichAddress: "Hangi adrese ne kadar bağış yapıldı?",
+        page: "Sayfa",
+        turkish: "Türkçe",
+        english: "İngilizce",
       },
     },
   },
@@ -214,11 +209,11 @@ const Home = () => {
           <img className="mt-24 sm:mt-10" src={ahbap} alt="" />
 
           <h1 className="text-xl sm:text-2xl font-semibold mt-8 max-w-7xl">
-            {t("Turkiye buyuk depremlerle sarsiliyor")}
+            {t("title")}
           </h1>
 
           <h2 className="text-base sm:text-xl mt-8 max-w-3xl">
-            {t("Merhaba, Ahbap araciligi ile kripto bagis topluyoruz.")}
+            {t("subTitle")}
           </h2>
 
           <Menu
@@ -234,7 +229,7 @@ const Home = () => {
                 />
 
                 <span>
-                  {i18n.language === "en" ? t("Ingilizce") : t("Turkce")}
+                  {i18n.language === "en" ? t("english") : t("turkish")}
                 </span>
 
                 <ChevronDownIcon
@@ -270,7 +265,7 @@ const Home = () => {
                       >
                         <img className="h-5 rounded-sm" src={tr} alt="" />
 
-                        <span>{t("Turkce")}</span>
+                        <span>{t("turkish")}</span>
                       </div>
                     )}
                   </Menu.Item>
@@ -290,7 +285,7 @@ const Home = () => {
                       >
                         <img className="h-5 rounded-sm" src={en} alt="" />
 
-                        <span>{t("Ingilizce")}</span>
+                        <span>{t("english")}</span>
                       </div>
                     )}
                   </Menu.Item>
@@ -318,20 +313,18 @@ const Home = () => {
             </div>
 
             <div className="mt-5 flex flex-col gap-y-4">
-              <span>{t("Bakiye")}: 11.048940004436707746 BNB</span>
-              <span>{t("BNB Degeri")}: $3,648.80 (@ $330.24/BNB)</span>
+              <span>{t("balance")}: 11.048940004436707746 BNB</span>
+              <span>{t("bnbValue")}: $3,648.80 (@ $330.24/BNB)</span>
             </div>
           </div>
 
           <div className="bg-white lg:w-[565px] border rounded shadow px-5 py-8 flex flex-col text-black text-left">
-            <h1 className="font-bold text-2xl">
-              {t("Transfer Yapilirken Dikkat Edilmesi Gerekenler")}
-            </h1>
+            <h1 className="font-bold text-2xl">{t("thingsToConsider")}</h1>
 
             <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-8">
               <div className="space-y-2">
                 <h1 className="font-bold">
-                  {t("Kripto Agi", {
+                  {t("cryptoNetwork", {
                     kripto: "ERC20",
                   })}
                 </h1>
@@ -371,11 +364,7 @@ const Home = () => {
                   })}
                 </h1>
 
-                <p className="">
-                  {t(
-                    "Tum coinler destekleniyor fakat yollarken BEP20 agini secin"
-                  )}
-                </p>
+                <p className="">{t("cryptoNetworkWarning")}</p>
               </div>
             </div>
           </div>
@@ -384,7 +373,7 @@ const Home = () => {
 
       <div className="mt-[72px] flex flex-col gap-y-2 max-w-6xl mx-auto px-8 lg:px-0">
         <h1 className="text-2xl font-bold">
-          {t("Hangi adrese ne kadar bagis yapildi?")}
+          {t("whichAddress")}
         </h1>
 
         <div>
@@ -419,13 +408,13 @@ const Home = () => {
                 setPage((page) => page - 1);
               }
             }}
-            className="h-10 w-10 border rounded inline-flex items-center justify-center font-bold hover:bg-gray-100 transition-colors cursor-pointer disabled:opacity-50 disabled:pointer-events-none transition-opacity"
+            className="h-10 w-10 border rounded inline-flex items-center justify-center font-bold hover:bg-gray-100 transition-all cursor-pointer disabled:opacity-50 disabled:pointer-events-none transition-opacity"
           >
             <ChevronLeftIcon className="h-4" />
           </button>
 
           <span>
-            {page}. {t("Sayfa")}
+            {page}. {t("page")}
           </span>
 
           <button
